9a69930091c9d841f30b26239fe61359b8067080
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-09-28 23:04:35 +0530

    cors imported

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const userRoute = require('./routes/UserRoute');
 const balanceRoute = require('./routes/balanceRoute');
