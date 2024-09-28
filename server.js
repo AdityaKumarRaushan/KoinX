65e0a1204707e860b8ab6619febc599f4bf2d498
Aditya Kumar Raushan <aditya21bcy42@iiitkottayam.ac.in>
2024-09-28 22:13:17 +0530

    router has been added in the server.js file and path is also set for post request

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const userRoute = require('./routes/UserRoute');
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI)
@@ -10,6 +12,8 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const app = express();
 app.use(express.json());
+
+app.use('/v1', userRoute);
 
 app.get('/', (req, res) => {
     return res.json({
